5825ce53a7331caa558ffe0ec6bb5fd5967c4d2c
Manasa Rayachoti <57994923+Manasa2811@users.noreply.github.com>
2021-12-06 21:30:51 +0530

    Update datanode.py

--- datanode.py
+++ datanode.py
@@ -6,7 +6,7 @@ from rpyc.utils.server import ThreadedServer
 
 class DatanodeServer(rpyc.Service):
 	class exposed_Datanode():		
-if __name__ == "__main__":
-	connection = rpyc.connect('localhost', port = 18812, config={"allow_all_attrs": True})
-	st = ThreadedServer(DatanodeServer, port = 8888)
-	st.start()
+		if __name__ == "__main__":
+			connection = rpyc.connect('localhost', port = 18812, config={"allow_all_attrs": True})
+			st = ThreadedServer(DatanodeServer, port = 8888)
+			st.start()
